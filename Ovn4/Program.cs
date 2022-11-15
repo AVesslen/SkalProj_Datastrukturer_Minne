137179555a9e49e8141ff4eb843228b95500e83f
Anna Vesslén <anna_ananaz@hotmail.com>
2022-11-15 12:57:02 +0100

    Added ExamineQueue method

--- Ovn4/Program.cs
+++ Ovn4/Program.cs
@@ -81,30 +81,8 @@ namespace SkalProj_Datastrukturer_Minne
         /// Examines the datastructure List
         /// </summary>
         static void ExamineList()
-        {
-            /*
-             * Loop this method untill the user inputs something to exit to main menue.
-             * Create a switch statement with cases '+' and '-'
-             * '+': Add the rest of the input to the list (The user could write +Adam and "Adam" would be added to the list)
-             * '-': Remove the rest of the input from the list (The user could write -Adam and "Adam" would be removed from the list)
-             * In both cases, look at the count and capacity of the list
-             * As a default case, tell them to use only + or -
-             * Below you can see some inspirational code to begin working.
-            */
-
-            //List<string> theList = new List<string>();
-            //string input = Console.ReadLine();
-            //char nav = input[0];
-            //string value = input.substring(1);
-
-            //switch(nav){...}
-
-            // Count is the number of elements that are present in the List
-            // Capacity is the number of elements that the List can store before resizing is required
-
-
-            List<string> theList = new List<string>();
-            Console.WriteLine($"Capacity default: {theList.Capacity}");
+        {      
+            List<string> theList = new List<string>();            
             bool isRunning = true;
             while (isRunning)
             {
@@ -147,19 +125,16 @@ namespace SkalProj_Datastrukturer_Minne
                         Console.WriteLine("Please, only enter \"+\" or \"-\" in front of the string.");
                         break;
                 }
-
-               
             }
 
             // Fråga 2. Listans kapacitet ökar först när den underliggande arrayen är full (count=capacity) och vi vill lägga till ett nytt element.
             // Fråga 3. Default kapacitet när första elementet läggs till är 4. Kapaciteten ökar sedan exponentiellt, genom att dubbleras när den
-            //          underliggande arrayen är full.
+            //          underliggande arrayen är full (4,8,16,32,64... osv.)
             // Fråga 4. Listans kapacitet ökar inte i samma takt som element läggs till pga att den lagrar värdena i en array. En array har en fix storlek,
             //          så att lägga till nya element är kostsamt. Det kräver att en ny, längre array skapas och de tidigare elementen kopieras över till
             //          den nya. 
             // Fråga 5. Kapaciteten minskar inte när element tas bort ur listan.
             // Fråga 6. När man vet på förhand hur många element man vill lägga till är det fördelaktigt att använda en egendefinierad array i stället för lista. 
-
         }
 
 
@@ -173,6 +148,52 @@ namespace SkalProj_Datastrukturer_Minne
              * Create a switch with cases to enqueue items or dequeue items
              * Make sure to look at the queue after Enqueueing and Dequeueing to see how it behaves
             */
+
+            List<string> theList = new List<string>();
+            Queue<string> theQueue= new Queue<string>();    
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine(" Enter a string with a \"+\" in front, if you want to enqueue (add item to the queue)."
+                     + "\n Enter a \"-\", it you want to eequeue (remove the first item from the queue)."
+                     + "\n Or enter 0 to exit to the main menu");
+
+                string input = Console.ReadLine();
+                char nav = input[0];
+                string value = input.Substring(1);
+
+                switch (nav)
+                {
+                    case '+':
+                        theQueue.Enqueue(value);
+
+                        Console.WriteLine($"Items in the list: ");
+                        foreach (var item in theQueue)
+                        {
+                            Console.WriteLine(item);
+                        }                       
+
+                        break;
+
+                    case '-':
+                        theQueue.Dequeue();
+                        Console.WriteLine($"Items in the list: ");
+                        foreach (var item in theQueue)
+                        {
+                            Console.WriteLine(item);
+                        }
+                       
+                        break;
+
+                    case '0':
+                        isRunning = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Please, only enter \"+\" or \"-\" in front of the string.");
+                        break;
+                }
+            }
         }
 
         /// <summary>
